10710ad01baaf66db79732591ac143396e87e395
dzabeligan <dzabeligan@gmail.com>
2020-11-28 16:26:58 +0100

    declare arguments in date helper functions as const

--- Transaction_Manager/utils.c
+++ Transaction_Manager/utils.c
@@ -7,7 +7,7 @@
  * isSameDay: Compares two dates. Returns true if dates *
  *           are equal, false otherwise                   *
  *********************************************************/
-bool isSameDay(int year, int month, int day, struct transaction *t)
+bool isSameDay(const int year, const int month, const int day, const struct transaction *t)
 {
   return year == t->year && month == t->month && day == t->day;
 }
@@ -17,7 +17,7 @@ bool isSameDay(int year, int month, int day, struct transaction *t)
  *           true if date in transaction is greater or    *
  *           equal to test date, false otherwise          *
  *********************************************************/
-bool isGreaterOrEqualDay(int year, int month, int day, struct transaction *t)
+bool isGreaterOrEqualDay(const int year, const int month, const int day, const struct transaction *t)
 {
   return t->year > year 
         || (t->year == year && t->month > month) 
@@ -29,7 +29,7 @@ bool isGreaterOrEqualDay(int year, int month, int day, struct transaction *t)
  *           true if date in transaction is lesser or     *
  *           equal to test date, false otherwise          *
  *********************************************************/
-bool isLesserOrEqualDay(int year, int month, int day, struct transaction *t)
+bool isLesserOrEqualDay(const int year, const int month, const int day, const struct transaction *t)
 {
   return t->year < year 
         || (t->year == year && t->month <= month) 
--- Transaction_Manager/utils.h
+++ Transaction_Manager/utils.h
@@ -3,8 +3,8 @@
 
 #include <stdbool.h>
 
-bool isSameDay(int year, int month, int day, struct transaction *t);
-bool isLesserOrEqualDay(int year, int month, int day, struct transaction *t);
-bool isGreaterOrEqualDay(int year, int month, int day, struct transaction *t);
+bool isSameDay(const int year, const int month, const int day, const struct transaction *t);
+bool isLesserOrEqualDay(const int year, const int month, const int day, const struct transaction *t);
+bool isGreaterOrEqualDay(const int year, const int month, const int day, const struct transaction *t);
 
 #endif // UTIL_H
